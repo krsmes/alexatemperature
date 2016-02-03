d1e232e58a15c01b7c07fa2840df9e267799c8a7
krsmes <krsmes@gmail.com>
2016-02-03 01:02:16 -0500

    added fixed get temperature response

--- amazon/src/main/groovy/net/krsmes/alexatemperature/GetTemperatureSpeechlet.groovy
+++ amazon/src/main/groovy/net/krsmes/alexatemperature/GetTemperatureSpeechlet.groovy
@@ -8,35 +8,46 @@ import com.amazon.speech.speechlet.SessionStartedRequest
 import com.amazon.speech.speechlet.Speechlet
 import com.amazon.speech.speechlet.SpeechletException
 import com.amazon.speech.speechlet.SpeechletResponse
+import com.amazon.speech.ui.Card
 import com.amazon.speech.ui.PlainTextOutputSpeech
 import com.amazon.speech.ui.Reprompt
 import com.amazon.speech.ui.SimpleCard
 
 class GetTemperatureSpeechlet implements Speechlet {
+    private static final SPEECH_ON_LAUNCH = 'Ask me what the temperature is'
+    private static final SPEECH_HELP_INTENT = 'You can ask what is the temperature, or how cold is it, or how hot is it'
+    private static final SPEECH_GET_TEMPERATURE = 'The temperature is 55 degrees fahrenheit'
+
     @Override
     void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
     }
 
     @Override
     SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
-        return greeting()
+        askResponse(SPEECH_ON_LAUNCH, new SimpleCard(title: 'GetTemperature'))
     }
 
     @Override
     SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
-        return null
+        switch (request?.intent?.name) {
+            case 'GetTemperatureIntent': tellResponse(SPEECH_GET_TEMPERATURE); break
+            case 'AMAZON.HelpIntent': askResponse(SPEECH_HELP_INTENT); break
+            default: null
+        }
     }
 
     @Override
     void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
     }
 
-    def greeting = {->
-        def speech = new PlainTextOutputSpeech(text: 'Ask me what the temperature is')
-        SpeechletResponse.newAskResponse(
-            speech,
-            new Reprompt(outputSpeech: speech),
-            new SimpleCard(title: 'GetTemperature')
-        )
+    def askResponse = { speechText, Card card = null ->
+        def speech = new PlainTextOutputSpeech(text: speechText)
+        card ? SpeechletResponse.newAskResponse(speech, new Reprompt(outputSpeech: speech), card)
+            : SpeechletResponse.newAskResponse(speech, new Reprompt(outputSpeech: speech))
+    }
+
+    def tellResponse = { speechText, Card card = null ->
+        def speech = new PlainTextOutputSpeech(text: speechText)
+        card ? SpeechletResponse.newTellResponse(speech, card) : SpeechletResponse.newTellResponse(speech)
     }
 }
--- amazon/src/test/groovy/net/krsmes/alexatemperature/GetTemperatureSpeechletSpec.groovy
+++ amazon/src/test/groovy/net/krsmes/alexatemperature/GetTemperatureSpeechletSpec.groovy
@@ -1,16 +1,26 @@
 package net.krsmes.alexatemperature
 
+import com.amazon.speech.slu.Intent
+import com.amazon.speech.speechlet.IntentRequest
 import com.amazon.speech.speechlet.SpeechletResponse
 import spock.lang.Specification
 
 class GetTemperatureSpeechletSpec extends Specification {
 
+    def subject = new GetTemperatureSpeechlet()
+    def request = null
+    def session = null
+
+    def intentRequest = { String name ->
+        IntentRequest.builder().withRequestId('requestid').withIntent(
+            Intent.builder().withName(name).build()
+        ).build()
+    }
+
+
     def "onLaunch should return greeting"() {
         given:
         def expectedResponseSpeech = 'Ask me what the temperature is'
-        def subject = new GetTemperatureSpeechlet()
-        def request = null
-        def session = null
         when:
         def response = subject.onLaunch(request, session)
         then:
@@ -18,6 +28,28 @@ class GetTemperatureSpeechletSpec extends Specification {
         response.outputSpeech.text == expectedResponseSpeech
         response.reprompt.outputSpeech.text == expectedResponseSpeech
         response.card.title == 'GetTemperature'
+    }
+
+    def "onIntent should return help response on help intent"() {
+        given:
+        def expectedResponseSpeech = 'You can ask what is the temperature, or how cold is it, or how hot is it'
+        request = intentRequest('AMAZON.HelpIntent')
+        when:
+        def response = subject.onIntent(request, session)
+        then:
+        response instanceof SpeechletResponse
+        response.outputSpeech.text == expectedResponseSpeech
+    }
+
+    def "onIntent should return temperature response on get temperature intent"() {
+        given:
+        def expectedResponseSpeech = 'The temperature is 55 degrees fahrenheit'
+        request = intentRequest('GetTemperatureIntent')
+        when:
+        def response = subject.onIntent(request, session)
+        then:
+        response instanceof SpeechletResponse
+        response.outputSpeech.text == expectedResponseSpeech
     }
 
 }
